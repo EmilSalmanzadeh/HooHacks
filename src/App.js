66d2a181b65ad97405eb385f2508b3f07a7403fd
EmilSalmanzadeh <esalmanz@ucsd.edu>
2021-03-27 18:26:04 -0700

    button skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import Button from '@material-ui/core/Button';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox, Goodbye. Testing 123456</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <a href="secondarypage.html">
+        <button>Button</button>
+      </a>
     </div>
   );
 }
