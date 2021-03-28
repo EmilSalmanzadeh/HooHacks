9ab9178be433e2e7fb9df0da22c91124b3ef9b14
EmilSalmanzadeh <esalmanz@ucsd.edu>
2021-03-27 20:07:31 -0700

    8:07 pm recording

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>HooHacks 2021</h1>
-      <h3>Sasan Salmanzadeh, Kristin Stacy, Emil Salmanzadeh</h3>
       <h2>Educational Science Application</h2>
       <img src="https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg" alt="Flower"></img>
       <br/><br/>
       <a href="secondarypage.html">
         <button>Start Game</button>
       </a>
+      <br/><br/>
+      <h3>Created by: Sasan Salmanzadeh, Kristin Stacy, Emil Salmanzadeh</h3>
     </div>
   );
 }
